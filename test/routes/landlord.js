cc25c22eb9d2d718db683c4d1da719b068593d72
suntala <suntala@hotmail.com>
2018-01-08 19:17:33 +0100

    Testing coverage at 98%

--- test/routes/landlord.js
+++ test/routes/landlord.js
@@ -2,6 +2,12 @@ import test from 'ava'
 import request from 'supertest'
 import app from '../../app'
 
+const LandlordModel = require('../../models/landlord-model')   //change to whatever model name you have
+const RenterModel = require('../../models/renter-model')  
+const PropertyModel = require('../../models/property-model') 
+const RenterService = require('../../services/renter-service') //format?
+const LandlordService = require('../../services/landlord-service')
+
 test('Get landlord page', async t => {
     const input = {name:'Test', money: 50, properties: []}
 
@@ -181,3 +187,83 @@ test('Property sale', async t => {
 // [ { name: 'TestLL', money: 380, properties: [ 'House' ] },
 //   { name: 'TestR', money: 1020, properties: [] } ]
 
+test('Delete a landlord', async t => {
+    const input = {name:'Test', money: 50, properties: []}
+
+    const creation = (await request(app)
+        .post('/landlord/add')
+        .send(input))
+        .body
+
+    // const fetch = await request(app)
+    //     .get(`/landlord/${creation.landlordID}`)
+    
+    // t.is(fetch.status, 200)
+
+    const res = await request(app)
+        .post('/landlord/delete')
+        .send(creation)
+
+    t.is(res.status, 200)
+
+    // const fetch = await request(app)
+    //     .get(`/landlord/${creation.landlordID}`)
+    
+    // t.is(fetch.status, 404)
+    /*
+    const fetch = await request(app)
+        .get(`/landlord/${creation.landlordID}`)
+
+    t.is(fetch.status, 404)   --> getting 500
+    */  
+})
+
+test('Pay rent', async t=> {
+    const inputLandlord = {name:'TestLL', money: 500, properties: []} //buyer
+    const inputProperty = {name:'TestP', cost: 10}
+    const inputRenter = {name:'TesterR', money: 50, months: ['Jan'], property: ["House"]}
+ 
+    const landlordCreation = (await request(app)
+        .post('/landlord/add')
+        .send(inputLandlord))
+        .body
+    
+
+    const propertyCreation = (await request(app)
+        .post('/property/add')
+        .send(inputProperty))
+        .body
+    
+    const renterCreation = (await request(app)
+        .post('/renter/add')
+        .send(inputRenter))
+        .body
+
+    const input = {landlordID: landlordCreation.landlordID, renterID: renterCreation.renterID, propertyID: propertyCreation.propertyID}
+    
+    // console.log(input)
+
+    // const payRent = async (landlordID, renterID, propertyID) => {
+    //     const property = await PropertyModel.findOne({ propertyID })
+    //     console.log(property)
+    //     const landlord = await LandlordModel.findOne({ landlordID })
+    //     console.log(landlord)
+    //     const renter = await RenterModel.findOne({ renterID })
+    //     console.log(renter)
+    //     const monthRent = property.cost
+    //     const newLandlordMoney = landlord.money + monthRent
+    //     const newRenterMoney = renter.money - monthRent
+    //     const newLandlord = await LandlordService.edit(landlordID, {name: landlord.name, money: newLandlordMoney, properties: landlord.properties})
+    //     const newRenter = await RenterService.edit(renterID, {name: renter.name, money: newRenterMoney, months: renter.months, property: renter.property})
+    //     console.log([newLandlord, newRenter])
+    //     return [newLandlord, newRenter]
+    // }
+
+    // payRent(input.landlordID, input.renterID, input.propertyID);
+
+    const res = await request(app)
+        .post('/landlord/pay-rent')
+        .send(input)
+    
+    t.is(res.status, 200)
+})
